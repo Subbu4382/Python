9e4bd77d72ead40a06022ea566fb1f6924e70cc5
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-01 11:36:19 +0530

    math module

---
+++ module_Math.py
@@ -0,0 +1,25 @@
+import math
+# finding square root
+print(math.sqrt(100))
+n=int(input("enter a number"))
+print(n**0.5)
+
+# finding power
+print(math.pow(5,2))
+print(n**2)
+
+#finding factorial
+print(math.factorial(5))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)
+
+# finding floor
+print(math.floor(12.5))
+print(12.8//1)
+
+# finding ceil
+print(math.ceil(12.6))
+print((19.6//1)+1)
+
