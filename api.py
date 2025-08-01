2820f52259a68604250871e23cae4d3bbb92f3a9
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-01 13:13:35 +0530

    api

---
+++ api.py
@@ -0,0 +1,22 @@
+import requests,os
+api="https://fakestoreapi.in/api/products"
+data=requests.get(api)
+# print(data)
+products=data.json()
+# print(products)
+
+# open powershell as administrator
+# set-ExecutionPolicy RemoteSigned
+try:
+  for i in products["products"]:
+    # print(i["id"],i["title"])
+      print(i["id"],i["brand"],i["model"],i["price"],i["color"])
+    #   raise KeyError("Key not found")
+except:
+   print("Key not exists")
+# except Exception as e:
+#    print(e)
+finally:
+   with open("files.py",'r') as f:
+     print(f.read())
+   print("executed successfully")
