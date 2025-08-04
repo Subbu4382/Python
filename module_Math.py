d4651242e835ddd442195eec9acbec196a224db8
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-08-04 21:23:04 +0530

    math module

--- module_Math.py
+++ module_Math.py
@@ -1,25 +1,39 @@
 import math
-# finding square root
-print(math.sqrt(100))
-n=int(input("enter a number"))
-print(n**0.5)
+# # finding square root
+# print(math.sqrt(100))
+# n=int(input("enter a number"))
+# print(n**0.5)
 
-# finding power
-print(math.pow(5,2))
-print(n**2)
+# # finding power
+# print(math.pow(5,2))
+# print(n**2)
 
-#finding factorial
-print(math.factorial(5))
-fact=1
-for i in range(1,n+1):
-    fact=fact*i
-print(fact)
+# #finding factorial
+# print(math.factorial(5))
+# fact=1
+# for i in range(1,n+1):
+#     fact=fact*i
+# print(fact)
 
-# finding floor
-print(math.floor(12.5))
-print(12.8//1)
+# # finding floor
+# print(math.floor(12.5))
+# print(12.8//1)
 
-# finding ceil
-print(math.ceil(12.6))
-print((19.6//1)+1)
+# # finding ceil
+# print(math.ceil(12.6))
+# print((19.6//1)+1)
 
+# Finding the value of pi
+print(math.pi)
+
+# Finding Eulers number 
+print(math.e)
+
+# Finding Natural logarithm
+print(math.log(50))
+
+# Finging sine angle in radians
+print(math.sin(90))
+
+# Finding Tan angle in radians
+print(math.tan(45))
